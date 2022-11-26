2a4d13a43fcc9487aa337afaad80f426bdd84019
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-11-26 10:11:41 -0300

    feat(api): added app error class #21

---
+++ packages/api/src/errors/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Represents a known error that occurred while processing a request.
+ */
+export class AppError {
+  public readonly message: string;
+
+  public readonly statusCode: number;
+
+  constructor(message: string, statusCode = 400) {
+    this.message = message;
+    this.statusCode = statusCode;
+  }
+}
