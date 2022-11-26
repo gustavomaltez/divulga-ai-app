f5e22b8c5bbec4ac182a47f5866f52863eb63276
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-11-26 10:10:31 -0300

    feat(api): added authentication helpers #23

--- packages/api/src/entities/index.ts
+++ packages/api/src/entities/index.ts
@@ -1,2 +1,3 @@
 export { User } from './User.entity';
+export { Rate } from './Rate.entity';
 export { Advertising } from './Advertising.entity';
---
+++ packages/api/src/utils/authentication.ts
@@ -0,0 +1,47 @@
+
+import { User } from '@entities';
+import jwt from 'jsonwebtoken';
+
+/**
+ * Generates a string access token associated to the provided user.
+ * 
+ * @param user The user data to generate the token for
+ * @returns A string token associated to the provided user
+ */
+export function generateUserAccessToken(user: User): string {
+  const { ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION_TIME } = process.env;
+  const payload = { id: user.id };
+  return jwt.sign(payload, ACCESS_TOKEN_SECRET as string, {
+    expiresIn: ACCESS_TOKEN_EXPIRATION_TIME
+  });
+}
+
+/**
+ * Generates a string refresh token associated to the provided user.
+ * 
+ * @param user The user data to generate the token for
+ * @returns A string token associated to the provided user
+ */
+export function generateUserRefreshToken(user: User): string {
+  const { REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION_TIME } = process.env;
+  const payload = { id: user.id };
+  return jwt.sign(payload, REFRESH_TOKEN_SECRET as string, {
+    expiresIn: REFRESH_TOKEN_EXPIRATION_TIME
+  });
+}
+
+/**
+ * Helper function to generate both access and refresh tokens.
+ * 
+ * @param user The user data to generate the tokens for
+ * @returns An object containing both the access and refresh tokens
+ */
+export function generateUserTokens(user: User): {
+  accessToken: string,
+  refreshToken: string;
+} {
+  return {
+    accessToken: generateUserAccessToken(user),
+    refreshToken: generateUserRefreshToken(user)
+  };
+}
